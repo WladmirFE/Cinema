3cfa8276f87047a25590527ad9c9f6d8a97f2693
WladmirFE <wladmirsilva13@gmail.com>
2024-07-23 17:07:14 -0300

    Aula 23/07/24

---
+++ loginControle.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST['email']) && isset($_POST['senha'])){
+
+    $sql = "where email = :email and senha = :senha ";
+    $arrayDeParametros = array(":email", ":senha");
+    $arrayDeValores = array($_POST['email'], $_POST['senha']);
+
+    require_once $_SERVER['DOCUMENT_ROOT']."/cinema/modelo/dao/UsuarioDAO.php";
+
+    $listaUsuario = UsuarioDAO::getInstance()->listWhere($sql, $arrayDeParametros, $arrayDeValores);
+
+    if(count($listaUsuario)>0){
+
+        session_start();
+        $_SESSION['idUsuarioLogado'] = $listaUsuario[0]->getId();
+        header("Location: ./UsuarioList.php");
+
+    }else{
+        header("Location: ./login.php?erro=1");
+    
+    }
+
+}
+
